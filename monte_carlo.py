74e5eacd1c9471483318d70c568328a1e50fb621
James Han <42987815+jayimshan@users.noreply.github.com>
2023-11-28 21:20:18 -0500

    Update monte_carlo.py
    Part B

--- monte_carlo.py
+++ monte_carlo.py
@@ -1,112 +1,124 @@
 import random
-from collections import Counter
+import numpy as np
 
-t = 100000 # number of simulations to run
+num_simulations = 10 # number of simulations to run
 n = 20 # size of network
 p = 0.1 # probability of infection
-d = 0 # number of days it takes to infect each computer once
-i = 1 # number of initial infection
+r = 5 # max quantity of repairs
 
 class Computer:
-    def __init__(self, infected, p, infected_once):
-        self.infected = infected
-        self.p = p
-        self.infected_once = infected_once  
+    infected = False
+    infected_once = False
+    p = 0.1 # probability of being infected
+    
+    def infect(self):
+        self.infected = True
+        self.infected_once = True
 
-def main():
-    # network = create_network(n) # create a network of computers of size n
-    # initial_infection(i, network) # assign initial number of computers to be infected
-    # print('Expected value of %.2f computers infected in the network of %d computers in %d trials with %.2f probability of infection' %(expected_value(t, n, i), n, t, p))
-    # print(infect_all(network, d))
-    infected_once(t, i, d)
+    def repair(self):
+        self.infected = False
+        
 
 def create_network(network_size):
-    # print('Creating network of size: %d' %(network_size))
-    i = 0
     network = []
-    while i < network_size:
-        network.append(Computer(False, p, False))
-        i += 1
+    for i in range(network_size):
+        network.append(Computer())
     return network
 
-def initial_infection(num_infect, network):
-    # print('Initial number of infected computers: %d' %(num_infect))
-    i = 0
-    for computer in network:
-        if i >= num_infect:
-            break
-        else:
-            computer.infected = True
-            computer.infected_once = True
-            i += 1
+def initial_infection(network, quantity):
+    # check if quantity is less than network size
+    for i in range(quantity):
+        network[i].infect()
+    return network
 
 def daily_infection(network):
-    infected = 0
-    not_infected = 0
+    num_infected = 0
+    # count number of infected computers
     for computer in network:
         if computer.infected:
-            infected += 1
-        else:
+            num_infected += 1
+    
+    # for each infected computer, spread virus throughout network
+    for _ in range(num_infected):
+        for computer in network:
             u = random.random()
             if u < computer.p:
-                computer.infected = True
-                computer.infected_once = True
-                infected += 1
-            else:
-                not_infected += 1
-    d = {}
-    d['infected'] = infected
-    d["not_infected"] = not_infected
-    return d
+                computer.infect()
+    return network
 
-def infect_all(network, days):
-    daily_infection(network)
-    days += 1
+def number_infected(network):
+    i = 0
     for computer in network:
-        if not computer.infected_once:
-            return infect_all(network, days)
-    return days
+        if computer.infected:
+            i += 1
+    # print(i)
+    return i
 
-def daily_repair(network):
+def number_infected_once(network):
+    i = 0
+    for computer in network:
+        if computer.infected_once:
+            i += 1
+    # print(i)
+    return i
+
+def daily_repair(network):  
     i = 0 # current number of repairs
-    max_repair = 5 # maximum number of repairs
     for computer in network:
-        if i >= max_repair:
-            break
-        else:
-            if computer.infected:
-                computer.infected = False
-                i += 1
-                # print('Computer repaired...')
+        if computer.infected and i < r:
+            computer.repair()
+            i += 1
+    # print('Repaired a total of %d computers' %(i))
+    return network
 
-def expected_value(num_trials, network_size, num_infect):
-    i = 0
-    infected = 0.0
-    not_infected = 0.0
-    d = {}
-    print('Running %d trials...' %(num_trials))
-    while i < num_trials:
-        network = create_network(network_size)
-        initial_infection(num_infect, network)
-        temp_dict = daily_infection(network)
-        # add daily repair
-        infected += temp_dict['infected']
-        not_infected += temp_dict['not_infected']
-        i += 1
-    d['infected'] = infected
-    d['not_infected'] = not_infected
-    # print(d)
-    return infected / num_trials
+def expected_number_infected():
+    total_number_infected_computers = 0
+    for _ in range(num_simulations):
+        network = create_network(n) # create new network of size n
+        network = initial_infection(network, 1) # initial infection
+        network = daily_infection(network) # daily infection simulation
+        total_number_infected_computers += number_infected(network) # sum number of infected computers per trial
 
-def infected_once(num_trials, num_infect, d):
-    i = 0
-    days = 0
-    while i < num_trials:
-        # print(infect_all(network, d))
+    print('%.4f' %(total_number_infected_computers / num_simulations)) # expected number of infected computers
+
+def probability_infected_once():
+    total_number_infected_once = 0
+    for _ in range(num_simulations):
+        network = create_network(n) # create new network of size n
+        network = initial_infection(network, 1) # initial infection
+        num_current_infected = 0
+        while num_current_infected < n:
+            network = daily_infection(network) # daily infection simulation
+            num_current_infected = number_infected(network)
+            if num_current_infected == n or num_current_infected == 0:
+                break
+            network = daily_repair(network) # daily repair
+            # current_infected = number_infected(network)
+        if number_infected_once(network) == n:
+            total_number_infected_once += 1
+        
+    # print(total_number_infected_once)
+    print('%.4f' %(total_number_infected_once / num_simulations))
+
+def remove_virus():
+    elapsed_days = 0
+    for _ in range(num_simulations):
         network = create_network(n)
-        initial_infection(num_infect, network)
-        days += infect_all(network, d)
-        i += 1
-    print('Expected number of days that each computer is infected at least once: %.2f' %(days / i))
+        network = initial_infection(network, 1)
+        # network = initial_infection(network, random.randint(1, n)) # start with random number of initially infected for this to work
+        num_current_infected = number_infected(network)
+        while num_current_infected > 0:
+            network = daily_infection(network)
+            print('Infected: %d' %(number_infected(network)))
+            network = daily_repair(network)
+            print('Infected after repair: %d' %(number_infected(network)))
+            num_current_infected = number_infected(network)
+            elapsed_days += 1
+        
+        print('Elapsed days: %d' %(elapsed_days))
+    print('%.4f' %(elapsed_days / num_simulations))
+    # print('%.4f' %(np.mean(elapsed_days)))
 
-main()
+# expected_number_infected()
+# probability_infected_once()
+remove_virus()
